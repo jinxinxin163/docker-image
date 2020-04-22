c4d698112a2ababcf5fee40649100e1f07b72b74
root <jin.xin@advantech.com.cn>
2020-04-22 09:42:55 +0800

    +indices.fielddata.cache.size:  40%

--- elasticsearch/build.sh
+++ elasticsearch/build.sh
@@ -2,7 +2,7 @@
 TARGET=elasticsearch
 #REPO=registry.cn-shanghai.aliyuncs.com/advantech-k8s/
 REPO=harbor.arfa.wise-paas.com/ensaas-logging/
-TAG=7.6.0-1
+TAG=7.6.0-2
 docker build -t ${REPO}${TARGET}:${TAG} -f ./Dockerfile .
 docker push ${REPO}${TARGET}:${TAG}
 
