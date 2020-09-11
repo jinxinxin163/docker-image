e7e5386eb0b893bd9cc0fc8f17400c37f04b507f
root <jin.xin@advantech.com.cn>
2020-09-11 08:29:34 +0800

    add backup for cluster backup

---
+++ backup/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TARGET=schedule
+REPO=registry.cn-shanghai.aliyuncs.com/advantech-k8s/
+#REPO=harbor.arfa.wise-paas.com/ensaas-logging/
+TAG=1.0.1
+docker build -t ${REPO}${TARGET}:${TAG} -f ./Dockerfile .
+docker push ${REPO}${TARGET}:${TAG}
+
