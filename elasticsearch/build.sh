2cd66e4264e5746188965c6aa77c9231ce8c0890
root <jin.xin@advantech.com.cn>
2021-03-04 17:22:30 +0800

    elasticsearch support es-heaad

--- elasticsearch/build.sh
+++ elasticsearch/build.sh
@@ -2,7 +2,7 @@
 TARGET=elasticsearch
 #REPO=registry.cn-shanghai.aliyuncs.com/advantech-k8s/
 REPO=harbor.arfa.wise-paas.com/ensaas-logging/
-TAG=7.6.0-3
+TAG=7.6.0-4
 docker build -t ${REPO}${TARGET}:${TAG} -f ./Dockerfile .
 docker push ${REPO}${TARGET}:${TAG}
 
